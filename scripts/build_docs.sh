caa212c2caf3ab8a5f031307142bddeca64a0a30
Doug Koellmer <dougkoellmer@hotmail.com>
2014-11-11 11:39:20 -0500

    Changed doc generation script to remove old library code name.

--- scripts/build_docs.sh
+++ scripts/build_docs.sh
@@ -2,9 +2,9 @@ cd ../src
 
 #CSS_PATH=$ANDROID_HOME/extras/google/google_play_services/docs/assets/android-developer-docs.css
 #CSS_PATH=$ANDROID_HOME/docs/assets/android-developer-docs.css
-CSS_PATH=../assets/doc_style.css
+#CSS_PATH=../assets/doc_style.css
 
 
-javadoc -protected -stylesheetfile $CSS_PATH -windowtitle BtFresh -author -d ../doc/api com.idevicesllc.btfresh
+javadoc -protected -windowtitle SweetBlue -author -d ../doc/api com.idevicesinc.sweetblue
 
 cd -
