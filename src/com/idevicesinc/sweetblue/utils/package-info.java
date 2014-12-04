5a716e70e7e36a034753f8b2dc96964e6deeabda
Doug Koellmer <dougkoellmer@hotmail.com>
2014-12-04 15:58:46 -0500

    Small API doc change.

--- src/com/idevicesinc/sweetblue/utils/package-info.java
+++ src/com/idevicesinc/sweetblue/utils/package-info.java
@@ -1,4 +1,4 @@
 /**
- * Mostly contains a few optional utilities that you may find useful.
+ * Contains a few optional utilities that you may find useful.
  */
 package com.idevicesinc.sweetblue.utils;
