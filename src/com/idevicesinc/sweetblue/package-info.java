9057c6160509aa1d78236a94462084768d632283
Doug Koellmer <dougkoellmer@hotmail.com>
2014-12-04 16:14:29 -0500

    Small API doc change.

--- src/com/idevicesinc/sweetblue/package-info.java
+++ src/com/idevicesinc/sweetblue/package-info.java
@@ -1,4 +1,5 @@
 /**
- * Contains the core public-facing classes {@link BleDevice} and {@link BleManager} and most of the internal guts.
+ * Contains the core public-facing classes {@link BleDevice} and {@link BleManager}
+ * and most of the internal guts for SweetBlue, the easiest way to do BLE on Android.
  */
 package com.idevicesinc.sweetblue;
