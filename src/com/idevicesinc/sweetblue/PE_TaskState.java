4e53b1ccadf32cf0100c711dc727eea64fef9062
Doug Koellmer <dougkoellmer@hotmail.com>
2014-12-05 11:01:18 -0500

    Code comment refactoring.

--- src/com/idevicesinc/sweetblue/PE_TaskState.java
+++ src/com/idevicesinc/sweetblue/PE_TaskState.java
@@ -7,10 +7,10 @@ package com.idevicesinc.sweetblue;
  */
 enum PE_TaskState
 {
-	CREATED,	// very transient, not really listened for at the moment.
-	QUEUED,		// task is in queue...previous state can be CREATED or INTERRUPTED
+	CREATED,				// very transient, not really listened for at the moment.
+	QUEUED,					// task is in queue...previous state can be CREATED or INTERRUPTED
 	
-	ARMED,		// task is current but not yet executing...there's a configurable time for being in this state but usually ends up just being one timeStep. 
+	ARMED,					// task is current but not yet executing...there's a configurable time for being in this state but usually ends up just being one timeStep. 
 	EXECUTING,
 	
 	// ending states
@@ -18,7 +18,7 @@ enum PE_TaskState
 	TIMED_OUT,
 	INTERRUPTED,			// put back on queue...next state will be QUEUED.
 	CANCELLED,
-	SOFTLY_CANCELLED, // set after arming (preemptively cancels execution) or is mutated from the SUCCEEDED state if task is softly cancelled while already executing.
+	SOFTLY_CANCELLED,		// set after arming (preemptively cancels execution) or is mutated from the SUCCEEDED state if task is softly cancelled while already executing.
 	FAILED,
 	CLEARED_FROM_QUEUE,
 	REDUNDANT,
