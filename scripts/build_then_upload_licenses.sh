67d483cdba732c6fea83c0839930cbafb7f056c9
Doug Koellmer <dougkoellmer@hotmail.com>
2015-03-19 10:46:18 -0400

    Licensing changes.

--- scripts/build_then_upload_licenses.sh
+++ scripts/build_then_upload_licenses.sh
@@ -5,4 +5,4 @@ sh build_licenses.sh
 source ./config_license.sh
 
 SERVER_ADDRESS="162.209.102.219"
-sshpass -p $SWEETBLUE_COM_FTP_PASSWORD scp -rp $OUTDIR "${SWEETBLUE_COM_FTP_USERNAME}@${SERVER_ADDRESS}:/var/www/html/sweetblue"
+sshpass -p $SWEETBLUE_COM_FTP_PASSWORD scp -rp $OUTDIR/* "${SWEETBLUE_COM_FTP_USERNAME}@${SERVER_ADDRESS}:/var/www/html/sweetblue/licenses"
