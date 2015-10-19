e8f505881b44460643b74d40751d64e25b3ec3aa
Ryan Bis <ryanbis@idevicesinc.com>
2015-10-19 13:14:38 -0400

    couple more build script fixes

--- scripts/build_release_bundle_then_upload.sh
+++ scripts/build_release_bundle_then_upload.sh
@@ -2,17 +2,21 @@
 
 source config.sh
 ver=$(sh echo_version.sh)
+SWEETBLUE_DIR=sweetblue_${ver}
+SWEETBLUE_DOCS_DIR=$SWEETBLUE_DIR/docs/api/
 JAR_NAME=sweetblue_$ver
 sh gradlew fullBuild
 cp $STAGE/sweetblue.zip $STAGE/$JAR_NAME.zip
 read -n 1 -p "Are you sure you want to upload? " sure
 if [ "$sure" = "y" -o "$sure" = "Y" ];
 	then
-		echo "\n${GLITZ}UPLOADING ZIPS TO SERVER${GLITZ}"
+		echo "\n${GLITZ} UPLOADING ZIPS TO SERVER ${GLITZ}"
 	    cd $STAGE
 	    SERVER_ADDRESS="162.209.102.219"	     
     	sshpass -p $SWEETBLUE_COM_FTP_PASSWORD scp -p $JAR_NAME.zip "${SWEETBLUE_COM_FTP_USERNAME}@${SERVER_ADDRESS}:/var/www/html/sweetblue/downloads"
 	    sshpass -p $SWEETBLUE_COM_FTP_PASSWORD scp -p sweetblue.zip "${SWEETBLUE_COM_FTP_USERNAME}@${SERVER_ADDRESS}:/var/www/html/sweetblue/downloads"
+	    echo "\n${GLITZ} UPLOADING DOCS TO SERVER ${GLITZ}"
+		sshpass -p $SWEETBLUE_COM_FTP_PASSWORD scp -rp $SWEETBLUE_DOCS_DIR "${SWEETBLUE_COM_FTP_USERNAME}@${SERVER_ADDRESS}:/var/www/html/sweetblue/docs"
     	cd -
     	if [ "$?" != 0 ];
 		then	
