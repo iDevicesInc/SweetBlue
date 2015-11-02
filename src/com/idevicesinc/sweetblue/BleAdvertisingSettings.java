65cf50317c68e35841cb6f381cff9a8fada62473
Ryan Bis <ryanbis@idevicesinc.com>
2015-11-02 10:47:36 -0500

    update some API docs

--- src/com/idevicesinc/sweetblue/BleAdvertisingSettings.java
+++ src/com/idevicesinc/sweetblue/BleAdvertisingSettings.java
@@ -2,23 +2,22 @@ package com.idevicesinc.sweetblue;
 
 
 import android.bluetooth.le.AdvertiseSettings;
-
 import com.idevicesinc.sweetblue.utils.Interval;
 
+/**
+ * Class used specify Ble Advertising settings.
+ */
 public class BleAdvertisingSettings {
 
     /**
      * Type-safe parallel of static final int members of {@link android.bluetooth.le.AdvertiseSettings}.
-     * Provide an instance to {@link BleServer#startAdvertising(BleAdvertisingPacket, BleAdvertisingMode, BleTransmissionPower)},
-     * {@link BleServer#startAdvertising(BleAdvertisingPacket, BleAdvertisingMode, BleTransmissionPower, Interval)}, or
-     * {@link BleServer#startAdvertising(BleAdvertisingPacket, BleAdvertisingMode, BleTransmissionPower, Interval, BleServer.AdvertisingListener)}
      * This is only applicable for Android >= 5.0 OS levels.
      */
     public enum BleAdvertisingMode {
 
         /**
          * This option is recommended and will let SweetBlue automatically choose what advertising mode to use
-         * based on whether the app is backgrounded, if we're doing a continuous, or short-term advertisement
+         * based on whether the app is backgrounded, and if we're doing a continuous, or short-term advertisement
          */
         AUTO(-1),
 
@@ -55,9 +54,6 @@ public class BleAdvertisingSettings {
 
     /**
      * Type-safe parallel of static final int members of {@link android.bluetooth.le.AdvertiseSettings}.
-     * Provide an instance to {@link BleServer#startAdvertising(BleAdvertisingPacket, BleAdvertisingSettings.BleAdvertisingMode, BleAdvertisingSettings.BleTransmissionPower)},
-     * {@link BleServer#startAdvertising(BleAdvertisingPacket, com.idevicesinc.sweetblue.BleAdvertisingMode, BleTransmissionPower, Interval)}, or
-     * {@link BleServer#startAdvertising(BleAdvertisingPacket, com.idevicesinc.sweetblue.BleAdvertisingMode, BleTransmissionPower, Interval, BleServer.AdvertisingListener)}
      * This is only applicable for Android >= 5.0 OS levels.
      */
     public enum BleTransmissionPower {
@@ -103,37 +99,68 @@ public class BleAdvertisingSettings {
     private final BleTransmissionPower m_power;
     private final Interval m_timeout;
 
-
+    /**
+     * Base constructor which sets all relevant Ble advertising settings
+     */
     public BleAdvertisingSettings(BleAdvertisingMode mode, BleTransmissionPower power, Interval timeout) {
         m_mode = mode;
         m_power = power;
         m_timeout = timeout;
     }
 
+    /**
+     * Overload of {@link #BleAdvertisingSettings(BleAdvertisingMode, BleTransmissionPower, Interval)}, which sets
+     * the {@link com.idevicesinc.sweetblue.BleAdvertisingSettings.BleAdvertisingMode} to {@link com.idevicesinc.sweetblue.BleAdvertisingSettings.BleAdvertisingMode#AUTO},
+     * {@link com.idevicesinc.sweetblue.BleAdvertisingSettings.BleTransmissionPower} to {@link com.idevicesinc.sweetblue.BleAdvertisingSettings.BleTransmissionPower@MEDIUM},
+     * and the timeout to {@link Interval#ZERO} (no timeout).
+     */
     public BleAdvertisingSettings() {
         this(BleAdvertisingMode.AUTO, BleTransmissionPower.MEDIUM, Interval.ZERO);
     }
 
+    /**
+     * Overload of {@link #BleAdvertisingSettings(BleAdvertisingMode, BleTransmissionPower, Interval)}, which sets the
+     * {@link com.idevicesinc.sweetblue.BleAdvertisingSettings.BleAdvertisingMode} to {@link com.idevicesinc.sweetblue.BleAdvertisingSettings.BleAdvertisingMode#AUTO}.
+     */
     public BleAdvertisingSettings(BleTransmissionPower power, Interval timeout) {
         this(BleAdvertisingMode.AUTO, power, timeout);
     }
 
+    /**
+     * Overload of {@link #BleAdvertisingSettings(BleAdvertisingMode, BleTransmissionPower, Interval)}, which sets the
+     * {@link com.idevicesinc.sweetblue.BleAdvertisingSettings.BleAdvertisingMode} to {@link com.idevicesinc.sweetblue.BleAdvertisingSettings.BleAdvertisingMode#AUTO},
+     * and the timeout to {@link Interval#ZERO} (no timeout).
+     */
     public BleAdvertisingSettings(BleTransmissionPower power) {
         this(BleAdvertisingMode.AUTO, power, Interval.ZERO);
     }
 
+    /**
+     * Overload of {@link #BleAdvertisingSettings(BleAdvertisingMode, BleTransmissionPower, Interval)}, which sets the
+     * {@link com.idevicesinc.sweetblue.BleAdvertisingSettings.BleAdvertisingMode} to {@link com.idevicesinc.sweetblue.BleAdvertisingSettings.BleAdvertisingMode#AUTO}, and
+     * {@link com.idevicesinc.sweetblue.BleAdvertisingSettings.BleTransmissionPower} to {@link com.idevicesinc.sweetblue.BleAdvertisingSettings.BleTransmissionPower@MEDIUM}.
+     */
     public BleAdvertisingSettings(Interval timeout) {
         this(BleAdvertisingMode.AUTO, BleTransmissionPower.MEDIUM, timeout);
     }
 
+    /**
+     * Returns the advertising mode specified with {@link com.idevicesinc.sweetblue.BleAdvertisingSettings.BleAdvertisingMode}
+     */
     public BleAdvertisingMode getAdvertisingMode() {
         return m_mode;
     }
 
+    /**
+     * Returns the advertising transmission power specified with {@link com.idevicesinc.sweetblue.BleAdvertisingSettings.BleTransmissionPower}
+     */
     public BleTransmissionPower getTransmissionPower() {
         return m_power;
     }
 
+    /**
+     * Returns the timeout period.
+     */
     public Interval getTimeout() {
         return m_timeout;
     }
