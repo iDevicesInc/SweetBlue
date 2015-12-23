1094e60f23a4f185dff69cb48b7f5adc7ef3934a
Ryan Bis <ryanbis@idevicesinc.com>
2015-12-23 13:39:05 -0500

    more changes

--- scripts/build_release_bundle.sh
+++ scripts/build_release_bundle.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 source ./config_paths.sh
-#ANDROID_API_LEVEL=23
-#SWEETBLUE_SRC_PATH=../src
-#STAGE=script_output
-#JAR_NAME=sweetblue_$SEMVER
-#JAVADOC_JAR_NAME="sweetblue_${SEMVER}-javadoc"
-#SOURCES_JAR_NAME="sweetblue_${SEMVER}-sources"
-#BUNDLE_FOLDER=$STAGE/$JAR_NAME
 
 ARG=$1
 
-#./gradlew -b lint.gradle :lint
+./gradlew -b lint.gradle :lint
 
 if [ "$?" != 0 ];
 then
@@ -20,16 +13,16 @@ fi
 
 if [ "$ARG" == "info" ]
 then
-	sh gradlew fullBuild --info
+	./gradlew fullBuild --info
 	cp $STAGE/sweetblue.zip $STAGE/$JAR_NAME.zip
 elif [ "$ARG" == "stacktrace" ]
 then
-	sh gradlew fullBuild --stacktrace
+	./gradlew fullBuild --stacktrace
 	cp $STAGE/sweetblue.zip $STAGE/$JAR_NAME.zip
 elif [ "$ARG" == "no_zip" ]
 then
-	sh gradlew bundleNoZip
+	./gradlew bundleNoZip
 else
-	sh gradlew fullBuild
+	./gradlew fullBuild
 	cp $STAGE/sweetblue.zip $STAGE/$JAR_NAME.zip	
 fi
