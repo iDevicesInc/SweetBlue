6a7c2d0ca90b61c3f668108bf3b696668eaf395c
Ryan Bis <ryanbis@idevicesinc.com>
2016-01-20 12:01:55 -0500

    closes #95, will be in next release

--- src/com/idevicesinc/sweetblue/P_Task_Scan.java
+++ src/com/idevicesinc/sweetblue/P_Task_Scan.java
@@ -450,7 +450,7 @@ class P_Task_Scan extends PA_Task_RequiresBleOn
 	{
 		if( this.getState() == PE_TaskState.EXECUTING  )
 		{
-			if( getTotalTimeExecuting() >= getMinimumScanTime() && getQueue().getSize() > 0 && isSelfInterruptableBy(getQueue().peek()) )
+			if( getTotalTimeExecuting() >= getMinimumScanTime() || (getQueue().getSize() > 0 && isSelfInterruptableBy(getQueue().peek())) )
 			{
 				selfInterrupt();
 			}
@@ -473,7 +473,7 @@ class P_Task_Scan extends PA_Task_RequiresBleOn
 
 	private boolean isSelfInterruptableBy(final PA_Task otherTask)
 	{
-		if( otherTask.getPriority().ordinal() > PE_TaskPriority.FOR_NORMAL_READS_WRITES.ordinal() )
+		if( otherTask.getPriority().ordinal() > PE_TaskPriority.TRIVIAL.ordinal() )
 		{
 			return true;
 		}
