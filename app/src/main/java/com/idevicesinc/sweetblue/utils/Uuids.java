7d98402fab4a024f9fcd3a358dc706c93c125dce
Ryan Bis <ryanbis@idevicesinc.com>
2017-03-09 15:25:46 -0500

    bump version

--- app/src/main/java/com/idevicesinc/sweetblue/utils/Uuids.java
+++ app/src/main/java/com/idevicesinc/sweetblue/utils/Uuids.java
@@ -58,7 +58,7 @@ public final class Uuids
 	public static final UUID CHARACTERISTIC_PRESENTATION_FORMAT_DESCRIPTOR_UUID		= fromShort("2904");
 	
 
-	protected final static String BLUETOOTH_CONNECTED_HASH = "35452669a0050efc6823a77c0fc8f455fbe570eb";
+	protected final static String BLUETOOTH_CONNECTED_HASH = "06c4938be8e598602770461061af590f85a5cc8d";
 
 	
 	/**
