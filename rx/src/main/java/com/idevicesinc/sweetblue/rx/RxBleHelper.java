8453191a118176e3fcd28ebf9ab25b123a83f64c
Ryan Bis <ryanbis@idevicesinc.com>
2017-04-12 15:10:10 -0400

    add rx module

---
+++ rx/src/main/java/com/idevicesinc/sweetblue/rx/RxBleHelper.java
@@ -0,0 +1,15 @@
+package com.idevicesinc.sweetblue.rx;
+
+
+import com.idevicesinc.sweetblue.BleDevice;
+
+import io.reactivex.Observable;
+
+public class RxBleHelper
+{
+
+    public static Observable<BleDevice> scan(ScanOptions options) {
+
+    }
+
+}
