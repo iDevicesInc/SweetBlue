87f17226b191bee29ecdf6a636fabef700c2b79c
Noah Del Coro <noahdelcoro@idevicesinc.com>
2017-06-19 11:24:15 -0400

    Added more tests

--- integration/src/androidTest/java/com/idevicesinc/sweetblue/ScanTest.java
+++ integration/src/androidTest/java/com/idevicesinc/sweetblue/ScanTest.java
@@ -4,6 +4,8 @@ package com.idevicesinc.sweetblue;
 import android.util.Log;
 import com.idevicesinc.sweetblue.utils.Interval;
 import org.junit.Test;
+
+import java.util.StringTokenizer;
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
@@ -18,45 +20,75 @@ public class ScanTest extends BaseTester<MainActivity>
     @Test
     public void preLollipopScanTest() throws Exception
     {
-        doScanTest(Interval.TEN_SECS, BleScanApi.PRE_LOLLIPOP, BleScanPower.AUTO, Interval.secs(BleManagerConfig.DEFAULT_SCAN_REPORT_DELAY));
+        doScanTest("preL", Interval.TEN_SECS, BleScanApi.PRE_LOLLIPOP, BleScanPower.AUTO, Interval.secs(BleManagerConfig.DEFAULT_SCAN_REPORT_DELAY), Interval.secs(.5));
     }
 
     @Test
     public void postLollipopMediumTest() throws Exception
     {
-        doScanTest(Interval.TEN_SECS, BleScanApi.POST_LOLLIPOP, BleScanPower.MEDIUM_POWER, Interval.secs(BleManagerConfig.DEFAULT_SCAN_REPORT_DELAY));
+        doScanTest("postLMed", Interval.TEN_SECS, BleScanApi.POST_LOLLIPOP, BleScanPower.MEDIUM_POWER, Interval.secs(BleManagerConfig.DEFAULT_SCAN_REPORT_DELAY), Interval.secs(.5));
     }
 
     @Test
     public void postLollipopMediumNoBatchDelayTest() throws Exception
     {
-        doScanTest(Interval.TEN_SECS, BleScanApi.POST_LOLLIPOP, BleScanPower.MEDIUM_POWER, Interval.DISABLED);
+        doScanTest("postLMedNoBatch", Interval.TEN_SECS, BleScanApi.POST_LOLLIPOP, BleScanPower.MEDIUM_POWER, Interval.DISABLED, Interval.secs(.5));
     }
 
     @Test
     public void postLollipopHighTest() throws Exception
     {
-        doScanTest(Interval.TEN_SECS, BleScanApi.POST_LOLLIPOP, BleScanPower.HIGH_POWER, Interval.secs(BleManagerConfig.DEFAULT_SCAN_REPORT_DELAY));
+        doScanTest("postLHigh", Interval.TEN_SECS, BleScanApi.POST_LOLLIPOP, BleScanPower.HIGH_POWER, Interval.secs(BleManagerConfig.DEFAULT_SCAN_REPORT_DELAY), Interval.secs(.5));
     }
 
     @Test
     public void postLollipopHighNoBatchDelayTest() throws Exception
     {
-        doScanTest(Interval.TEN_SECS, BleScanApi.POST_LOLLIPOP, BleScanPower.HIGH_POWER, Interval.DISABLED);
+        doScanTest("postLHighNoBatch", Interval.TEN_SECS, BleScanApi.POST_LOLLIPOP, BleScanPower.HIGH_POWER, Interval.DISABLED, Interval.secs(.5));
     }
 
+    @Test
+    public void preLollipopScanTestNoBoost() throws Exception
+    {
+        doScanTest("preLNoBoost", Interval.TEN_SECS, BleScanApi.PRE_LOLLIPOP, BleScanPower.AUTO, Interval.secs(BleManagerConfig.DEFAULT_SCAN_REPORT_DELAY), Interval.DISABLED);
+    }
 
+    @Test
+    public void postLollipopMediumTestNoBoost() throws Exception
+    {
+        doScanTest("postLMedNoBoost", Interval.TEN_SECS, BleScanApi.POST_LOLLIPOP, BleScanPower.MEDIUM_POWER, Interval.secs(BleManagerConfig.DEFAULT_SCAN_REPORT_DELAY), Interval.DISABLED);
+    }
 
-    private void doScanTest(Interval scanLength, BleScanApi scanApi, BleScanPower scanPower, Interval batchDelay) throws Exception
+    @Test
+    public void postLollipopMediumNoBatchDelayTestNoBoost() throws Exception
+    {
+        doScanTest("postLMedNoBatchNoBoost", Interval.TEN_SECS, BleScanApi.POST_LOLLIPOP, BleScanPower.MEDIUM_POWER, Interval.DISABLED, Interval.DISABLED);
+    }
+
+    @Test
+    public void postLollipopHighTestNoBoost() throws Exception
+    {
+        doScanTest("postLHighNoBoost", Interval.TEN_SECS, BleScanApi.POST_LOLLIPOP, BleScanPower.HIGH_POWER, Interval.secs(BleManagerConfig.DEFAULT_SCAN_REPORT_DELAY), Interval.DISABLED);
+    }
+
+    @Test
+    public void postLollipopHighNoBatchDelayTestNoBoost() throws Exception
+    {
+        doScanTest("postLHighNoBatchNoBoost", Interval.TEN_SECS, BleScanApi.POST_LOLLIPOP, BleScanPower.HIGH_POWER, Interval.DISABLED, Interval.DISABLED);
+    }
+
+    private void doScanTest(final String name, Interval scanLength, BleScanApi scanApi, BleScanPower scanPower, Interval batchDelay, Interval boostScanTime) throws Exception
     {
         final Semaphore s = new Semaphore(0);
 
         final AtomicInteger deviceCount = new AtomicInteger(0);
         final AtomicLong timeStarted = new AtomicLong(0);
         final AtomicLong timeBeforeFirstDeviceDiscovered = new AtomicLong(0);
+        final AtomicInteger weakestRSSI = new AtomicInteger(0);
         mConfig.scanApi = scanApi;
         mConfig.scanPower = scanPower;
         mConfig.scanReportDelay = batchDelay;
+        mConfig.scanClassicBoostLength = boostScanTime;
         mgr.setConfig(mConfig);
 
         mgr.setListener_Discovery(new BleManager.DiscoveryListener()
@@ -71,6 +103,11 @@ public class ScanTest extends BaseTester<MainActivity>
                         timeBeforeFirstDeviceDiscovered.set(System.currentTimeMillis() - timeStarted.get());
                     }
                     deviceCount.incrementAndGet();
+
+                    if(e.rssi() < weakestRSSI.get())
+                    {
+                        weakestRSSI.set(e.rssi());
+                    }
                 }
             }
         });
@@ -82,8 +119,11 @@ public class ScanTest extends BaseTester<MainActivity>
             {
                 if (e.didExit(BleManagerState.SCANNING))
                 {
-                    Log.e(TAG, "Pre-Lollipop scan:\nTime to find first device: " + timeBeforeFirstDeviceDiscovered.get() + "ms\n" +
-                            "Scan finished with " + deviceCount.get() + " devices found.");
+                    Log.e(name+TAG, "\t" +
+                            (System.currentTimeMillis() - timeStarted.get()) + "\t" +  // Total time
+                            timeBeforeFirstDeviceDiscovered.get() + "\t" +  // Time to discover 1st device
+                            deviceCount.get() + "\t" +  // # of devices found
+                            weakestRSSI.get());  // Weakest RSSI discovered
                     s.release();
                 }
                 else if (e.didEnter(BleManagerState.SCANNING))
@@ -96,7 +136,6 @@ public class ScanTest extends BaseTester<MainActivity>
         mgr.startScan(scanLength);
         s.acquire();
     }
-
 
     @Override
     Class<MainActivity> getActivityClass()
