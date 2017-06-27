199fcf2b02c638d58b22067a6b4a609a565901c9
David Layne <davidlayne@idmbp-C02MG5CXFD56-DL.local>
2017-06-27 12:52:03 -0400

    Added a message when a service has no characteristics Sorted characteristics so they are sorted by Read, Read/Write, Write Fixed some cell refresh issues

--- toolbox/src/main/java/com/idevicesinc/sweetblue/toolbox/activity/BleCharacteristicsActivity.java
+++ toolbox/src/main/java/com/idevicesinc/sweetblue/toolbox/activity/BleCharacteristicsActivity.java
@@ -7,7 +7,10 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBar;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.ExpandableListView;
+import android.widget.TextView;
+
 import com.idevicesinc.sweetblue.BleDevice;
 import com.idevicesinc.sweetblue.BleManager;
 import com.idevicesinc.sweetblue.toolbox.R;
@@ -22,11 +25,10 @@ public class BleCharacteristicsActivity extends BaseActivity
     private BleDevice m_device;
     private BluetoothGattService m_service;
     private List<BluetoothGattCharacteristic> m_characteristicList;
+    private TextView m_noCharacteristicsTextView;
 
     private CharacteristicAdapter m_adapter;
     private ExpandableListView m_charListView;
-
-
 
     @Override protected void onCreate(@Nullable Bundle savedInstanceState)
     {
@@ -53,5 +55,12 @@ public class BleCharacteristicsActivity extends BaseActivity
 
         m_charListView.setAdapter(m_adapter);
 
+        boolean haveCharacteristics = !m_characteristicList.isEmpty();
+
+        m_charListView.setVisibility(haveCharacteristics ? View.VISIBLE : View.GONE);
+
+        m_noCharacteristicsTextView = find(R.id.noCharacteristicsTextView);
+
+        m_noCharacteristicsTextView.setVisibility(haveCharacteristics ? View.GONE : View.VISIBLE);
     }
 }
