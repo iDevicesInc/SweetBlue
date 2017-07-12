21e2bd8db207ea68db78d6daa719b42656af08ab
Ryan Bis <ryanbis@idevicesinc.com>
2017-07-12 13:07:43 -0400

    Add unit test, fix getChar method to use new logic

--- tester/src/test/java/com/idevicesinc/sweetblue/DuplicateCharTest.java
+++ tester/src/test/java/com/idevicesinc/sweetblue/DuplicateCharTest.java
@@ -12,6 +12,8 @@ import org.robolectric.annotation.Config;
 import java.util.Arrays;
 import java.util.UUID;
 import java.util.concurrent.Semaphore;
+
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 
@@ -23,6 +25,7 @@ public class DuplicateCharTest extends BaseBleUnitTest
     private final static UUID mTestService = Uuids.fromShort("ABCD");
     private final static UUID mTestChar = Uuids.fromShort("1234");
     private final static UUID mTestDesc = Uuids.CHARACTERISTIC_PRESENTATION_FORMAT_DESCRIPTOR_UUID;
+    private final static UUID mNotifyDesc = Uuids.CLIENT_CHARACTERISTIC_CONFIGURATION_DESCRIPTOR_UUID;
 
     private BleDevice m_device;
 
@@ -31,6 +34,11 @@ public class DuplicateCharTest extends BaseBleUnitTest
             .addDescriptor(mTestDesc).setValue(new byte[] { 0x1 }).setPermissions().read().completeChar()
             .addCharacteristic(mTestChar).setValue(new byte[] { 0x2, 0x3, 0x4, 0x5, 0x6 }).setProperties().readWriteNotify().setPermissions().readWrite().build()
             .addDescriptor(mTestDesc).setValue(new byte[] { 0x2 }).setPermissions().read().completeService();
+
+    private GattDatabase db2 = new GattDatabase().addService(mTestService)
+            .addCharacteristic(mTestChar).setValue(new byte[] { 0x2, 0x3, 0x4, 0x5, 0x6 }).setProperties().readWriteNotify().setPermissions().readWrite().build()
+            .addDescriptor(mNotifyDesc).setPermissions().readWrite().completeChar()
+            .addCharacteristic(mTestChar).setValue(new byte[] { 0x2, 0x3, 0x4, 0x5, 0x6 }).setProperties().readWrite().setPermissions().readWrite().completeService();
 
 
     @Test
@@ -118,6 +126,70 @@ public class DuplicateCharTest extends BaseBleUnitTest
                                     {
                                         assertTrue(e.status().name(), e.wasSuccess());
                                         assertTrue(e.characteristic().getDescriptor(mTestDesc).getValue()[0] == 2);
+                                        assertTrue(Arrays.equals(e.data(), new byte[] { 0x2, 0x3, 0x4, 0x5, 0x6 }));
+                                        succeed();
+                                    }
+                                });
+                            }
+                        }
+                    });
+                }
+            }
+        });
+
+        m_mgr.newDevice(Util.randomMacAddress(), "Test Device");
+
+        startTest();
+
+    }
+
+    @Test
+    public void readCharWhenMultipleExistOutOfSpecTest() throws Exception
+    {
+        m_device = null;
+
+        m_config.gattLayerFactory = new P_GattLayerFactory()
+        {
+            @Override
+            public P_GattLayer newInstance(BleDevice device)
+            {
+                return new UnitTestGatt(device, db2);
+            }
+        };
+
+        m_mgr.setConfig(m_config);
+
+        m_mgr.setListener_Discovery(new BleManager.DiscoveryListener()
+        {
+            @Override public void onEvent(DiscoveryEvent e)
+            {
+                if (e.was(LifeCycle.DISCOVERED))
+                {
+                    m_device = e.device();
+                    m_device.connect(new BleDevice.StateListener()
+                    {
+                        @Override public void onEvent(StateEvent e)
+                        {
+                            if (e.didEnter(BleDeviceState.INITIALIZED))
+                            {
+                                m_device.read(mTestChar, new DescriptorFilter()
+                                {
+                                    @Override public Please onEvent(DescriptorEvent event)
+                                    {
+                                        // we're looking to read the char withOUT the notification descriptor
+                                        return Please.acceptIf(event.characteristic().getDescriptor(mNotifyDesc) == null);
+                                    }
+
+                                    @Override public UUID descriptorUuid()
+                                    {
+                                        return null;
+                                    }
+                                }, new BleDevice.ReadWriteListener()
+                                {
+                                    @Override public void onEvent(ReadWriteEvent e)
+                                    {
+                                        assertTrue(e.status().name(), e.wasSuccess());
+                                        assertNull(e.characteristic().getDescriptor(mNotifyDesc));
                                         assertTrue(Arrays.equals(e.data(), new byte[] { 0x2, 0x3, 0x4, 0x5, 0x6 }));
                                         succeed();
                                     }
