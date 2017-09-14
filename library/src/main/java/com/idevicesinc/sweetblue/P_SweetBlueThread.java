2d2b47bf5916353bbd7cde1e37fea2b7a62989a6
David Layne <davidlayne@idmbp-C02MG5CXFD56-DL.local>
2017-09-14 14:44:36 -0400

    Removed force sleep from sweet blue thread, trying yield instead

--- library/src/main/java/com/idevicesinc/sweetblue/P_SweetBlueThread.java
+++ library/src/main/java/com/idevicesinc/sweetblue/P_SweetBlueThread.java
@@ -24,7 +24,7 @@ final class P_SweetBlueThread implements P_SweetHandler
 
     @Override public void post(Runnable action)
     {
-        m_runnables.add(new SweetRunnable(action, System.currentTimeMillis(), 0));
+        m_runnables.add(new SweetRunnable(action, System.currentTimeMillis()));
     }
 
     @Override public void postDelayed(Runnable action, long delay)
@@ -69,43 +69,48 @@ final class P_SweetBlueThread implements P_SweetHandler
     {
         private final Runnable m_runnable;
         private final long m_postedTime;
-        private final long m_delay;
-        private boolean m_canceled;
+        private final Long m_delay;
+        private boolean m_canceled = false;
 
+        public SweetRunnable(Runnable action, long postedTime)
+        {
+            this(action, postedTime, null);
+        }
 
-        public SweetRunnable(Runnable action, long postedTime, long delay)
+        public SweetRunnable(Runnable action, long postedTime, Long delay)
         {
             m_runnable = action;
             m_postedTime = postedTime;
             m_delay = delay;
         }
 
-        public void run()
+        synchronized public void run()
         {
-            if (!m_canceled)
-            {
-                m_runnable.run();
-            }
+            if (m_canceled)
+                return;
+            m_runnable.run();
         }
 
-        public void cancel()
+        synchronized public void cancel()
         {
             m_canceled = true;
         }
 
-        public boolean canceled()
+        synchronized public boolean getCanceled()
         {
             return m_canceled;
         }
 
-        public boolean ready(long curTime)
+        synchronized public boolean ready(long curTime)
         {
-            return m_canceled || (curTime - m_postedTime) > m_delay;
+            if (m_canceled || m_delay == null)
+                return true;
+            return (curTime - m_postedTime) > m_delay;
         }
 
-        public boolean ready()
+        synchronized public boolean ready()
         {
-            return m_canceled || ready(System.currentTimeMillis());
+            return ready(System.currentTimeMillis());
         }
     }
 
@@ -121,21 +126,23 @@ final class P_SweetBlueThread implements P_SweetHandler
                     Iterator<SweetRunnable> it = m_runnables.iterator();
                     while (it.hasNext())
                     {
+                        if (!m_running)
+                            break;
+
                         SweetRunnable run = it.next();
-                        if (run.ready(curTime))
-                        {
-                            it.remove();
-                            run.run();
-                        }
+
+                        // Skip any tasks that aren't ready
+                        if (!run.ready())
+                            continue;
+
+                        // Run the task then remove it.  If the task was already canceled, it will not perform it's run operation
+                        run.run();
+                        it.remove();
                     }
                 }
+
                 // Sleep for a short period, so we don't hog the cpu
-                try
-                {
-                    Thread.sleep(5);
-                } catch (Exception e)
-                {
-                }
+                Thread.yield();
             }
         }
     }
