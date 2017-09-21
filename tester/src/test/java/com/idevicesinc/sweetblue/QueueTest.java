fa1db169d4d83cfce08f289a7c8de1b4b7de4387
David Layne <davidlayne@idmbp-C02MG5CXFD56-DL.local>
2017-09-21 16:40:37 -0400

    Extended queue test a bit

--- tester/src/test/java/com/idevicesinc/sweetblue/QueueTest.java
+++ tester/src/test/java/com/idevicesinc/sweetblue/QueueTest.java
@@ -7,6 +7,7 @@ import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 
+import java.util.List;
 import java.util.Random;
 
 @Config(manifest = Config.NONE, sdk = 25)
@@ -129,7 +130,7 @@ public class QueueTest extends BaseBleUnitTest
         }
     }
 
-    private int populateQueue(int numTasks, double distributionRatio)
+    private int populateQueue(int numTasks, double distributionRatio, int startingOrdinal)
     {
         Random r = new Random();
 
@@ -142,14 +143,14 @@ public class QueueTest extends BaseBleUnitTest
         {
             TestTask tt = null;
 
-            if (r.nextDouble() < .5)
+            if (r.nextDouble() <= distributionRatio)
             {
-                tt = new TestTaskA(m_mgr, i);
+                tt = new TestTaskA(m_mgr, startingOrdinal + i);
                 ++countA;
             }
             else
             {
-                tt = new TestTaskB(m_mgr, i);
+                tt = new TestTaskB(m_mgr, startingOrdinal + i);
                 ++countB;
             }
 
@@ -163,9 +164,10 @@ public class QueueTest extends BaseBleUnitTest
     }
 
 
-    @Test
+    @Test//(timeout = 30000)
     public void queueTest() throws Exception
     {
+        startSynchronousTest();
         m_config.loggingOptions = LogOptions.ON;
         m_mgr.setConfig(m_config);
 
@@ -174,12 +176,12 @@ public class QueueTest extends BaseBleUnitTest
         // Suspend the queue so we can cram it full of stuff w/o it doing anything just yet
         tm.setSuspended(true);
 
-        final int kTaskCount = 10000;
+        final int kTaskCount = 5000;
 
         mStartTimestamp = System.currentTimeMillis();
         System.out.println("Starting queue operations at " + mStartTimestamp);
 
-        int countA = populateQueue(kTaskCount, .5);
+        int countA = populateQueue(kTaskCount, .5, 0);
         int countB = kTaskCount - countA;
 
         long dt = System.currentTimeMillis() - mStartTimestamp;
@@ -192,10 +194,71 @@ public class QueueTest extends BaseBleUnitTest
         mStartTimestamp = System.currentTimeMillis();
         System.out.println("Queue operations complete after " + dt + " ms.  Queue size is now " + tm.getSize() + ".  Starting task processing at " + mStartTimestamp);
 
+        // Add some more tasks, favoring A more
+        countA = populateQueue(kTaskCount, .75, kTaskCount);
+        countB += kTaskCount - countA;
+
+        // Now, verify that there are the correct number of each task
+        int observedCountA = 0;
+        int observedCountB = 0;
+        List<PA_Task> l = tm.getRaw();
+
+        PE_TaskPriority prevPriority = null;
+        int prevOrdinal = 0;
+
+        for (PA_Task t : l)
+        {
+            TestTask tt = (TestTask)t;
+            if (t instanceof TestTaskA)
+                ++observedCountA;
+            if (t instanceof TestTaskB)
+                ++observedCountB;
+
+            PE_TaskPriority priority = t.getPriority();
+            int currentOrdinal = (Integer)tt.getMetadata();
+
+            // Make sure the priorities aren't broken
+            if (prevPriority != null)
+            {
+                if (prevPriority.ordinal() < priority.ordinal())
+                    assertTrue(false);
+
+                if (prevPriority.ordinal() == priority.ordinal())
+                {
+                    boolean expr = currentOrdinal > prevOrdinal;
+                    if (!expr)
+                    {
+                        System.out.println("oops");
+                    }
+                    assertTrue(currentOrdinal > prevOrdinal);
+                }
+            }
+
+            prevOrdinal = currentOrdinal;
+            prevPriority = priority;
+        }
+
+        assertTrue(observedCountA == countA);
+        assertTrue(observedCountB == countB);
+
+        succeed();
+    }
+
+    /*@Test
+    public void queueProcessTest() throws Exception
+    {
+        m_config.loggingOptions = LogOptions.ON;
+        m_mgr.setConfig(m_config);
+
+        P_TaskManager tm = m_mgr.getTaskManager();
+
         mRemainingTasks = tm.getSize();
 
         tm.setSuspended(false);
 
-        startAsyncTest();
-    }
+        // Suspend the queue so we can cram it full of stuff w/o it doing anything just yet
+        tm.setSuspended(true);
+
+    }*/
+
 }
