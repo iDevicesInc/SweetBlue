35a0ae6bc066ceff3203fa396c44e16d4de1a7de
Ryan <rbis@idevicesinc.com>
2018-04-05 12:32:39 -0400

    Wrap early out calls, so we can cache characteristics/descriptors. This fixes an issue where the early out check would be successful, but then later fail the operation because one of them was null.

---
+++ tester/src/test/java/com/idevicesinc/sweetblue/EarlyOutTest.java
@@ -0,0 +1,45 @@
+package com.idevicesinc.sweetblue;
+
+
+import com.idevicesinc.sweetblue.utils.Util;
+import com.idevicesinc.sweetblue.utils.Uuids;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+import static org.junit.Assert.assertTrue;
+
+
+@Config(manifest = Config.NONE, sdk = 25)
+@RunWith(RobolectricTestRunner.class)
+public class EarlyOutTest extends BaseBleUnitTest
+{
+
+    @Test
+    public void notConnectedEarlyOutTest() throws Exception
+    {
+        startTest(false);
+        BleDevice device = m_mgr.newDevice(Util.randomMacAddress());
+        BleDevice.ReadWriteListener.ReadWriteEvent event = device.read(Uuids.BATTERY_SERVICE_UUID);
+        assertTrue(event.status() == BleDevice.ReadWriteListener.Status.NOT_CONNECTED);
+    }
+
+    @Test
+    public void noMatchingTargetTest() throws Exception
+    {
+        BleDevice device = m_mgr.newDevice(Util.randomMacAddress());
+        device.connect(e ->
+        {
+            if (e.didEnter(BleDeviceState.INITIALIZED))
+            {
+                BleDevice.ReadWriteListener.ReadWriteEvent event = device.read(Uuids.BATTERY_SERVICE_UUID);
+                assertTrue(event.status() == BleDevice.ReadWriteListener.Status.NO_MATCHING_TARGET);
+                succeed();
+            }
+        });
+        startTest(true);
+    }
+
+}
