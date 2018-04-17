d4decd0e259574239018c16057c226a8925f3bed
Ryan <rbis@idevicesinc.com>
2018-04-17 08:43:35 -0400

    Add some javadoc to ManufacturerData class

--- library/src/main/java/com/idevicesinc/sweetblue/utils/ManufacturerData.java
+++ library/src/main/java/com/idevicesinc/sweetblue/utils/ManufacturerData.java
@@ -1,9 +1,20 @@
 package com.idevicesinc.sweetblue.utils;
 
+
+/**
+ * POJO to hold the manufacturer id, and manufacturer data parsed from a device's scan record.
+ */
 public class ManufacturerData
 {
 
+    /**
+     * The manufacturer id
+     */
     public short m_id;
+
+    /**
+     * Manufacturer data for the manufacturer id stored in {@link #m_id}
+     */
     public byte[] m_data;
 
 }
